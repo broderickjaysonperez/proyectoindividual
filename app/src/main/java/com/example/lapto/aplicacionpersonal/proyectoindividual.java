85791f021323ec6d1b0722b21ddde685a5806867
broderick <brodjay@hotmail.com>
2019-07-22 18:26:45 -0600

    commit 2 proyectoindividual

---
+++ app/src/main/java/com/example/lapto/aplicacionpersonal/proyectoindividual.java
@@ -0,0 +1,14 @@
+package com.example.lapto.aplicacionpersonal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class proyectoindividual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proyectoindividual);
+    }
+}
